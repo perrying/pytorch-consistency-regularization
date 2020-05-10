3a76c6d78c209c1244b03f5fa835db0fe8e0d44d
teppei suzuki <tsuzuki906@keio.jp>
2020-05-10 22:28:02 +0900

    modify ema for buffer parameters

--- ssl_lib/models/utils.py
+++ ssl_lib/models/utils.py
@@ -74,12 +74,12 @@ def __param_update(ema_model, raw_model, factor):
 
 
 def __buffer_update(ema_model, raw_model, factor):
-    # """ema for buffer parameters (e.g., running_mean and running_var in nn.BatchNorm2d)"""
-    # for ema_p, raw_p in zip(ema_model.buffers(), raw_model.buffers()):
-    #     ema_p.data = __ema(ema_p.data, raw_p.data, factor)
-    """copy buffer parameters (e.g., running_mean and running_var in nn.BatchNorm2d)"""
+    """ema for buffer parameters (e.g., running_mean and running_var in nn.BatchNorm2d)"""
     for ema_p, raw_p in zip(ema_model.buffers(), raw_model.buffers()):
-        ema_p.copy_(raw_p)
+        ema_p.data = __ema(ema_p.data, raw_p.data, factor)
+    # """copy buffer parameters (e.g., running_mean and running_var in nn.BatchNorm2d)"""
+    # for ema_p, raw_p in zip(ema_model.buffers(), raw_model.buffers()):
+    #     ema_p.copy_(raw_p)
 
 
 def ema_update(ema_model, raw_model, ema_factor, weight_decay_factor=None, global_step=None):
